852b2d22f63467b13220fc87de4717e0dc09243f
Varya-Deviaterikova <varya.deviaterikova@toptal.com>
2022-10-10 18:27:31 +0100

    feat: solve count tasks

--- app/count.js
+++ app/count.js
@@ -2,6 +2,22 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    let timeout;
 
+    const tick = () => {
+      console.log(start++);
+
+      if (start <= end) {
+        timeout = setTimeout(tick, 100);
+      }
+    }
+
+    tick();
+
+    return {
+      cancel: () => {
+        timeout && clearTimeout(timeout);
+      }
+    };
   }
 };
